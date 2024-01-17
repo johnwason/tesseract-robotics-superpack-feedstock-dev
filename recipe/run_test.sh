e69bc481991be34f2fb934651d3dcaff82457de6
John Wason <wason@wasontech.com>
2024-01-17 13:52:27 -0500

    Update recipe

--- recipe/run_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-export TESSERACT_SUPPORT_DIR=$CONDA_PREFIX/opt/tesseract_robotics/share/tesseract_support
-
-echo TESSERACT_SUPPORT_DIR=$TESSERACT_SUPPORT_DIR
-
-pytest -s
--- recipe/test_planning_server.py
+++ recipe/test_planning_server.py
@@ -7,12 +7,15 @@ import numpy.testing as nptest
 from tesseract_robotics.tesseract_common import ResourceLocator, SimpleLocatedResource
 from tesseract_robotics.tesseract_environment import Environment
 from tesseract_robotics.tesseract_common import FilesystemPath, Isometry3d, Translation3d, Quaterniond, \
-    ManipulatorInfo
-from tesseract_robotics.tesseract_command_language import JointWaypoint, CartesianWaypoint, Waypoint, \
-    MoveInstructionType_FREESPACE, MoveInstructionType_START, MoveInstruction, Instruction, \
-    isMoveInstruction, isStateWaypoint, CompositeInstruction, flatten, isMoveInstruction, isStateWaypoint
-from tesseract_robotics.tesseract_process_managers import ProcessPlanningServer, ProcessPlanningRequest, \
-    FREESPACE_PLANNER_NAME
+    ManipulatorInfo, AnyPoly, AnyPoly_wrap_double
+from tesseract_robotics.tesseract_command_language import CartesianWaypoint, WaypointPoly, \
+    MoveInstructionType_FREESPACE, MoveInstruction, InstructionPoly, StateWaypoint, StateWaypointPoly, \
+    CompositeInstruction, MoveInstructionPoly, CartesianWaypointPoly, ProfileDictionary, \
+        AnyPoly_as_CompositeInstruction, CompositeInstructionOrder_ORDERED, DEFAULT_PROFILE_KEY, \
+        AnyPoly_wrap_CompositeInstruction, DEFAULT_PROFILE_KEY, JointWaypoint, JointWaypointPoly, \
+        InstructionPoly_as_MoveInstructionPoly, WaypointPoly_as_StateWaypointPoly, \
+        MoveInstructionPoly_wrap_MoveInstruction, StateWaypointPoly_wrap_StateWaypoint, \
+        CartesianWaypointPoly_wrap_CartesianWaypoint, JointWaypointPoly_wrap_JointWaypoint
 
 class TesseractSupportResourceLocator(ResourceLocator):
     def __init__(self):
@@ -38,6 +41,16 @@ class TesseractSupportResourceLocator(ResourceLocator):
         except:
             traceback.print_exc()
 
+from tesseract_robotics.tesseract_task_composer import TaskComposerPluginFactory, \
+    TaskComposerDataStorage, TaskComposerInput, PlanningTaskComposerProblemUPtr, \
+    PlanningTaskComposerProblemUPtr_as_TaskComposerProblemUPtr
+
+OMPL_DEFAULT_NAMESPACE = "OMPLMotionPlannerTask"
+TRAJOPT_DEFAULT_NAMESPACE = "TrajOptMotionPlannerTask"
+
+TESSERACT_SUPPORT_DIR = os.environ["TESSERACT_SUPPORT_DIR"]
+TESSERACT_TASK_COMPOSER_DIR = os.environ["TESSERACT_TASK_COMPOSER_DIR"]
+
 def get_environment():
     locator = TesseractSupportResourceLocator()
     env = Environment()
@@ -52,43 +65,84 @@ def get_environment():
     
     return env, manip_info
 
-def test_planning_server_freespace():
-
-    env, manip = get_environment()
-
+def freespace_example_progam_iiwa(manipulator_info, goal = None, composite_profile = DEFAULT_PROFILE_KEY, 
+                                freespace_profile = DEFAULT_PROFILE_KEY):
+    if goal is None:
+        goal = Isometry3d.Identity() * Translation3d(0.2, 0.2, 1.0)
+    program = CompositeInstruction(DEFAULT_PROFILE_KEY, CompositeInstructionOrder_ORDERED, manipulator_info)
     joint_names = ["joint_a1", "joint_a2", "joint_a3", "joint_a4", "joint_a5", "joint_a6", "joint_a7"]
+    joint_values = np.zeros((7,))
+    wp1 = StateWaypointPoly_wrap_StateWaypoint(StateWaypoint(joint_names, joint_values))
+    start_instruction = MoveInstructionPoly_wrap_MoveInstruction(MoveInstruction(wp1, MoveInstructionType_FREESPACE, freespace_profile))
+    start_instruction.setDescription("Start Instruction")
 
-    wp1 = JointWaypoint(joint_names, np.array([0,0,0,-1.57,0,0,0],dtype=np.float64))
-    wp2 = CartesianWaypoint(Isometry3d.Identity() * Translation3d(-.2,.4,0.2) * Quaterniond(0,0,1.0,0))
+    wp2 = CartesianWaypointPoly_wrap_CartesianWaypoint(CartesianWaypoint(goal))
+    plan_f0 = MoveInstructionPoly_wrap_MoveInstruction(MoveInstruction(wp2, MoveInstructionType_FREESPACE, freespace_profile))
+    plan_f0.setDescription("freespace_motion")
+    program.appendMoveInstruction(start_instruction)
+    program.appendMoveInstruction(plan_f0)
 
-    start_instruction = MoveInstruction(Waypoint(wp1), MoveInstructionType_START, "DEFAULT")
-    plan_f1 = MoveInstruction(Waypoint(wp2), MoveInstructionType_FREESPACE, "DEFAULT")
+    wp3 = JointWaypointPoly_wrap_JointWaypoint(JointWaypoint(joint_names, np.zeros((7,))))
+    plan_f1 = MoveInstructionPoly_wrap_MoveInstruction(MoveInstruction(wp3, MoveInstructionType_FREESPACE))
+    program.appendMoveInstruction(plan_f1)
 
-    program = CompositeInstruction("DEFAULT")
-    program.setStartInstruction(Instruction(start_instruction))
-    program.setManipulatorInfo(manip)
-    program.append(Instruction(plan_f1))
+    return program
 
-    planning_server = ProcessPlanningServer(env, 1)
-    planning_server.loadDefaultProcessPlanners()
-    request = ProcessPlanningRequest()
-    request.name = FREESPACE_PLANNER_NAME
-    request.instructions = Instruction(program)
 
-    response = planning_server.run(request)
-    planning_server.waitForAll()
+def test_task_composer_trajopt_example():
+    env, manip_info = get_environment()
 
-    assert response.interface.isSuccessful()
+    config_path = FilesystemPath(os.path.join(TESSERACT_TASK_COMPOSER_DIR, "config/task_composer_plugins.yaml"))
+    factory = TaskComposerPluginFactory(config_path)
 
-    results = flatten(response.problem.getResults().as_CompositeInstruction())
+    task = factory.createTaskComposerNode("TrajOptPipeline")
+    
+    input_key = task.getInputKeys()[0]
+    output_key = task.getOutputKeys()[0]
 
-    # assert len(results) == 37
-    for instr in results:
-        assert isMoveInstruction(instr)
-        move_instr1 = instr.as_MoveInstruction()
-        wp1 = move_instr1.getWaypoint()
-        assert isStateWaypoint(wp1)
-        wp = wp1.as_StateWaypoint()
-        assert len(wp.joint_names) == 7
-        assert isinstance(wp.position,np.ndarray)
-        assert len(wp.position) == 7
+    profiles = ProfileDictionary()
+
+    program = freespace_example_progam_iiwa(manip_info)
+
+    program_anypoly = AnyPoly_wrap_CompositeInstruction(program)
+    task_data = TaskComposerDataStorage()
+    task_data.setData(input_key, program_anypoly)
+
+    planning_task_problem = PlanningTaskComposerProblemUPtr.make_unique(env, task_data, profiles)
+    task_problem = PlanningTaskComposerProblemUPtr_as_TaskComposerProblemUPtr(planning_task_problem)
+
+    task_input = TaskComposerInput(task_problem)
+    
+
+    task_executor = factory.createTaskComposerExecutor("TaskflowExecutor")
+
+    output_program = None
+    try:
+        future = task_executor.run(task.get(), task_input)
+        future.wait()
+
+        output_program = AnyPoly_as_CompositeInstruction(task_input.data_storage.getData(output_key))
+        assert len(output_program) == 11
+
+        # Print out the output program
+        for i in range(len(output_program)):
+            instr = output_program[i]
+            if instr.isMoveInstruction():
+                move_instr = InstructionPoly_as_MoveInstructionPoly(instr)
+                wp = move_instr.getWaypoint()
+                if wp.isStateWaypoint():
+                    state_wp = WaypointPoly_as_StateWaypointPoly(wp)
+                    print(f"State Waypoint {i}: {state_wp.getPosition()}")
+
+
+        print("Done")
+    finally:
+
+        # Cleanup memory to prevent segfault on exit
+        del task_problem
+        del task_input
+        del output_program
+        del future
+        del task_data
+        del task_executor
+        del task
